4daa6ee930aa46999c0608b9a39451321f40b5dc
moonjiyoung <umarg228@naver.com>
2024-05-01 16:22:06 +0900

    feat: text입력부분

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,9 +1,29 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+  Keyboard,
+} from 'react-native';
 
 function AddTodo() {
   const [text, setText] = useState('');
   //   console.log(text);
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss(); //현재 나타난 키보드를 닫는다.
+  };
+
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={require('../assets/icons/add_white/add_white.png')} />
+    </View>
+  );
 
   return (
     <View style={styles.block}>
@@ -12,23 +32,69 @@ function AddTodo() {
         style={styles.input}
         value={text}
         onChangeText={setText}
+        onSubmitEditing={onPress} //enter를 눌렀을때 호출되는함수
+        returnKeyType="done" //enter의 타입. 이거에따라 enter부분에 보이는 설명또는 아이콘이 바뀐다
       />
+      {/* {Platform.OS === 'ios' ? (
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.circleWrapper}>
+          <TouchableNativeFeedback>
+            <View style={styles.buttonStyle}>
+              <Image
+                source={require('../assets/icons/add_white/add_white.png')}
+              />
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      )} */}
+      {Platform.select({
+        ios: <TouchableOpacity activeOpacity={0.5}>{button}</TouchableOpacity>,
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
     paddingHorizontal: 16, //좌우측 padding값
     borderColor: '#bdbdbd',
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   input: {
+    flex: 1,
     fontSize: 16,
     paddingVertical: 8,
+  },
+  buttonStyle: {
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
   },
 });
 
